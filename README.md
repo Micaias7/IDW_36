9723d19ff9774685317d6f88dd39f056b6636630
German Cepeda <elrayofashion@gmail.comgit config --global user.email elrayofashion@gmail.com>
2025-10-23 18:20:39 -0300

    Restaurando git

--- README.md
+++
@@ -1,9 +0,0 @@
-# IDW_36
-
-Integrantes del grupo 36
-
-- Yamila Ayi
-- Emiliana Abril Baglioni
-- German Cepeda
-- Micaias Gutierrez
-- Brenda Sofia Arevalo
--- config/inicializarLocalStorage.js
+++
@@ -1,8 +0,0 @@
-import { medicosIniciales } from "./medicosIniciales.js";
-
-export const inicializarLocalStorage = () => {
-  if (!localStorage.getItem("medicos")) {
-    localStorage.setItem("medicos", JSON.stringify(medicosIniciales));
-    console.log("LocalStorage inicializado con médicos predeterminados");
-  }
-};
